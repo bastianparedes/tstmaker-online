b2a74de6ea5c6e1bcf568dc351300309414faf22
Bastián <bparedesp@falabella.cl>
2024-04-14 13:18:37 -0400

    Apunto de agregar

--- apps/backend/api/index.py
+++ apps/backend/api/index.py
@@ -6,7 +6,7 @@ import db
 app = flask.Flask(__name__)
 
 
-@app.route('/api/exercises', methods=['GET', 'POST', 'PUT'])
+@app.route('/api/exercises', methods=['GET', 'POST'])
 def getFullExercises():
   if (flask.request.method == 'GET'):
     # print('AYUDA', flask.request.args)
@@ -36,11 +36,7 @@ def getFullExercises():
         description=request_data['description'],
         code=request_data['code']
     )
-    print(
-        'AYUDA',
-        type(
-            db.Exercise.id.column_name),
-        db.Exercise.id.column_name)
+
     return {
         db.Exercise.id.column_name: new_user.id,
         db.Exercise.name.column_name: new_user.name,
@@ -61,7 +57,45 @@ def getFullExercises():
     query.execute()
     return True
 
-  return {}
+  return None
+
+@app.route('/api/exercises/<int:id>', methods=['GET', 'PUT'])
+def getSpecificExercise(id: int):
+  if (flask.request.method == 'GET'):
+    query = db.Exercise.select(
+        db.Exercise.id,
+        db.Exercise.name,
+        db.Exercise.description,
+        db.Exercise.code,
+        db.Exercise.last_modified_date
+      ).where(
+        db.Exercise.id == id
+      ).order_by(
+        db.Exercise.id)
+    exercises = []
+    for exercise in query:
+      exercises.append({
+          "id": exercise.id,
+          "name": exercise.name,
+          "description": exercise.description,
+          "code": exercise.code,
+          "last_modified_date": exercise.last_modified_date
+      })
+    return exercises
+
+  elif (flask.request.method == 'PUT'):
+    request_data = flask.request.get_json()
+
+    query = (db.Exercise.update({
+        db.Exercise.name: request_data['name'],
+        db.Exercise.description: request_data['description'],
+        db.Exercise.code: request_data['code'],
+        db.Exercise.last_modified_date: datetime.datetime.now()
+    }).where(db.Exercise.id == request_data['id']))
+    query.execute()
+    return None
+
+  return None
 
 
 @app.route('/api', methods=['GET'])
