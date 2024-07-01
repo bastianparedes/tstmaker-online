7534e291c2f8ffd391575ef4d2cc22039646b508
Bastián <bparedesp@falabella.cl>
2024-07-01 16:16:29 -0400

    -

--- apps/backend/api/classes/__tests__/test_latex.py
+++
@@ -1,9 +0,0 @@
-from api.classes.index import Latex
-
-
-def test_fraction():
-  assert Latex.fraction(1, 2) == r' \dfrac{1}{2} '
-
-
-def test_overline():
-  assert Latex.overline(1) == r' \overline{1} '
